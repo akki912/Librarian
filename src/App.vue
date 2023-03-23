a4751c05ed2952bc621a6dd6df30f40b9aa2a139
akki912 <aakash2001holikatti@gmail.com>
2023-03-23 11:31:57 +0530

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,57 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      
+      
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="@/assets/Librarian_Woman.jpg"
+          transition="scale-transition"
+          width="40"
+        />
+
+        {{ $const.APP_NAME }}
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://accounts.google.com/v3/signin/identifier?dsh=S1107838969%3A1678948649108238
+        &continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&
+        emr=1&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&
+        ifkv=AWnogHd5qp3P4Y1gn1pmDio8qsRdcpzYJjF0xs2frUEpFWtc60btg2tEEXPUOi0HVPI4aHkz9biB&
+        osid=1&passive=1209600&service=mail&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">LOGIN</span>
+        <v-icon>mdi-account-circle</v-icon> <!-- sourced this from 
+                                            VUE documentation app -->
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import url('@/assets/styles.css');
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="center">
+        <v-row>
+         <v-col cols="10" offset="1" sm="6" offset-sm="3" md="4" offset-md="4" > 
+            <v-avatar color="primary" size="100" tile>
+                <v-img src="@/assets/Librarian_Woman.jpg"> </v-img>
+            </v-avatar>
+            <v-text-field label="Username" v-model="username"></v-text-field>
+            <v-text-field label="Password" type="password" v-model="password"></v-text-field>
+            <v-btn block @click="authenticate()">Login</v-btn>
+            </v-col>
+    </v-row>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            username:"",
+            password:""
+        }
+    },
+
+    mdethods: {
+        authentication() {
+            alert:("Login Clicked")
+        }
+    },
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
